e6573a2db68778af47c4c93773a9bd52966448f2
saksham033 <sakshamgoyal02@gmail.com>
2019-01-09 18:40:25 +0000

    tissot: Initial PA Bringup.
    Signed-off-by: saksham033 <sakshamgoyal02@gmail.com>

--- pa_tissot.mk
+++ pa_tissot.mk
@@ -14,6 +14,13 @@
 # limitations under the License.
 #
 
+# Check for target product
+ifeq (pa_tissot,$(TARGET_PRODUCT))
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+include device/qcom/common/common.mk
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -21,12 +28,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := pa_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
@@ -40,3 +44,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+
+# Paranoid Android platform
+include vendor/pa/main.mk
+
+endif
+
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+add_lunch_combo pa_tissot-userdebug
